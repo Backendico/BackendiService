7401ff03506f1fed698a5b235dffe1dd7e3de707
Hossynkoala <Hossynassghary@gmail.com>
2020-11-11 19:13:36 +0330

    Add project files.

---
+++ BackendiService/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BackendiService
+{
+    public partial class BackendiService : ServiceBase
+    {
+
+        public BackendiService()
+        {
+            InitializeComponent();
+            this.ServiceName = "BackendiService";
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        protected override void OnContinue()
+        {
+
+        }
+
+
+    }
+}
