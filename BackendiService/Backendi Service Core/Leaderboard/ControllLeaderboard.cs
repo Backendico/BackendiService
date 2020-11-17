97ba05f598233083c751c95294002323f3b6bec1
Administrator <hossynassghary@gmail.com>
2020-11-17 11:36:19 +0330

    Remove Backups if null or time out after 1 Mon

--- BackendiService/Backendi Service Core/Leaderboard/ControllLeaderboard.cs
+++ BackendiService/Backendi Service Core/Leaderboard/ControllLeaderboard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -102,25 +103,45 @@ namespace BackendiService.Backendi_Service_Core.Leaderboard
             foreach (var InfoUser in Users)
             {
                 //find Studios
-                foreach (var Studios in InfoUser["Games"].AsBsonArray)
+                try
                 {
-                    var StudioSetting = await Client.GetDatabase(Studios.AsString).GetCollection<BsonDocument>("Setting").FindAsync(new BsonDocument { { "_id", "Setting" } }).Result.SingleAsync();
 
-                    //find all Leaderboard
-                    foreach (var Leaderboards in StudioSetting["List"].AsBsonDocument)
+                    foreach (var Studios in InfoUser["Games"].AsBsonArray)
                     {
-                        foreach (var EachLeaderboard in Leaderboards.Value.AsBsonDocument["Backups"].AsBsonDocument)
+                        var StudioSetting = await Client.GetDatabase(Studios.AsString).GetCollection<BsonDocument>("Setting").FindAsync(new BsonDocument { { "_id", "Setting" } }).Result.SingleAsync();
+                        //find all Leaderboard
+                        foreach (var Leaderboards in StudioSetting["Leaderboards"]["List"].AsBsonDocument)
                         {
-                            if (EachLeaderboard.Value["Detail"].ToLocalTime().AddMilliseconds(10) <= DateTime.Now)
+                            //singel Leaderboard
+                            foreach (var Leaderboard in Leaderboards.Value.AsBsonDocument)
                             {
-                                await Client.GetDatabase("Downs").GetCollection<BsonDocument>("Fuck").InsertOneAsync(new BsonDocument());
+                                if (Leaderboard.Name == "Backups")
+                                {
+                                    foreach (var Backup in Leaderboard.Value.AsBsonDocument)
+                                    {
+                                        if (Backup.Value.AsBsonDocument["List"].AsBsonDocument.ElementCount >= 1)
+                                        {
+                                            if (Backup.Value.AsBsonDocument["Detail"].AsBsonDocument["End"].ToLocalTime().AddMonths(1) <= DateTime.Now)
+                                            {
+                                                DeleteLeaderboard(StudioSetting["Setting"].AsBsonDocument["Creator"].ToString(), Studios.AsString, Leaderboards.Name, Backup.Name);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            DeleteLeaderboard(StudioSetting["Setting"].AsBsonDocument["Creator"].ToString(), Studios.AsString, Leaderboards.Name, Backup.Name);
+                                        }
+
+                                    }
+                                }
+
                             }
-                            else
-                            {
-                                await Client.GetDatabase("faild").GetCollection<BsonDocument>("Fuck").InsertOneAsync(new BsonDocument());
-                            }
+
                         }
                     }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
                 }
             }
 
@@ -140,6 +161,17 @@ namespace BackendiService.Backendi_Service_Core.Leaderboard
             await client.ExecuteAsync(request);
         }
 
-
+        public async void DeleteLeaderboard(string Token, string Studio, string NameLeaderboard, string Version)
+        {
+            var client = new RestClient("http://193.141.64.203/PageLeaderBoard/RemoveBackup");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.DELETE);
+            request.AddParameter("Token", Token);
+            request.AddParameter("Studio", Studio);
+            request.AddParameter("NameLeaderboard", NameLeaderboard);
+            request.AddParameter("Version", Version);
+            var Result = await client.ExecuteAsync(request);
+            Debug.WriteLine(Result.StatusCode);
+        }
     }
 }
